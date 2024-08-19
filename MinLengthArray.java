b55d8d13b995d6ecb549f6eec816d2c784c8daab
sai4coding <155803032+sai4coding@users.noreply.github.com>
2024-08-19 16:07:33 +0530

    Update MinLengthArray.java
    Inserted full functional code

--- MinLengthArray.java
+++ MinLengthArray.java
@@ -1,4 +1,27 @@
 package com.java.arrays;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class MinLengthArray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String[] st=new String[5];
+        for(int i=0;i<5;i++){
+            st[i]=sc.next();
+        }
+        System.out.println(Arrays.toString(st));
+
+
+        String maxString=st[0];
+        for(int i=0;i<5;i++){
+            if(st[i].length()>maxString.length()){
+                maxString=st[i];
+
+
+            }
+
+        }
+        System.out.println(maxString);
+    }
+
+
 }
